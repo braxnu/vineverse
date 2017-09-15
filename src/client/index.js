3c010078ca6b0676ed58c0756fecda5809444c9e
Baczewski, Karol (ELS-LON) <karol.baczewski@mendeley.com>
2017-09-15 11:46:38 +0100

    use d3 namespace

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,7 @@
-import { select } from 'd3'
-import { geoOrthographic, geoPath } from 'd3-geo'
-import { json as geoJson } from 'd3-request'
+import * as d3 from 'd3'
 import { feature as topojsonFeature } from 'topojson'
-import { timer } from 'd3'
 
-const canvas = select('#globe').append('canvas')
+const canvas = d3.select('#globe').append('canvas')
 const size = 800
 
 canvas.attr('style', 'width: 100vw;')
@@ -13,25 +10,25 @@ canvas.attr('height', size)
 
 const context = canvas.node().getContext('2d')
 
-const projection = geoOrthographic()
+const projection = d3.geoOrthographic()
   .scale(size / 2.1)
   .translate([size / 2, size / 2])
   .clipAngle(90)
 
-const path = geoPath()
+const path = d3.geoPath()
   .projection(projection)
   .context(context)
 
-geoJson('/world-110m.json', (err, data) => {
+d3.json('/world-110m.json', (err, data) => {
   const land = topojsonFeature(data, data.objects.land)
   const t0 = Date.now()
   const origin = [71, -22]
   const velocity = 0.005
 
-  timer(() => {
+  d3.timer(() => {
     const dt = Date.now() - t0
 
-    projection.rotate([velocity * dt + origin[0], origin[1]]);
+    projection.rotate([velocity * dt + origin[0], origin[1]])
     context.clearRect(0, 0, size, size)
     context.beginPath()
     path(land)
