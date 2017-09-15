5343b9df5c52b747e8464ca8a406038e0dd74dda
Karol Baczewski <braxnu@gmail.com>
2017-09-15 18:14:51 +0100

    basic authentication

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import usersRoute from './routes/users'
 import model from '../shared/model'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -8,8 +9,26 @@ const store = {
   users: model()
 }
 
+const protect = (req, res, next) => {
+  if (!res.locals.user) {
+    res.status(301).end()
+    return
+  }
+
+  next()
+}
+
 app.use(express.static('dist/assets'))
-app.use('/api/users', usersRoute({store}))
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const { token } = req.cookies
+
+  res.locals.user = token && store.users.list({token})[0]
+  next()
+})
+
+app.use('/api/users', protect, usersRoute({store}))
 
 app.use((req, res) => res.status(404).end())
 
