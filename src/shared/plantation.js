0c0356175b49790dae39a159e28ae229d869239b
Karol Baczewski <braxnu@gmail.com>
2017-11-05 15:04:26 +0000

    harvest timing logic

--- src/shared/plantation.js
+++ src/shared/plantation.js
@@ -3,10 +3,16 @@ import { realSecondsToGameDays, gameDaysToRealSeconds } from './gametime'
 export default class Plantation {
   constructor ({
     date = Date.now(),
-    maxAge
+    maxAge,
+    firstCropAfter,
+    harvestTime,
+    harvestEvery = 0
   }) {
     this.birthDate = date
-    this.maxAge = maxAge // game days
+    this.maxAge = maxAge // in game days
+    this.firstCropAfter = firstCropAfter // in game days
+    this.harvestTime = harvestTime // in game days
+    this.harvestEvery = harvestEvery // in game days
   }
 
   getAge (realNow = Date.now()) {
@@ -15,5 +21,15 @@ export default class Plantation {
 
   isAlive (realNow = Date.now()) {
     return realNow - this.birthDate <= gameDaysToRealSeconds(this.maxAge)
+  }
+
+  getAvailableCrop (realNow = Date.now()) {
+    let x = this.getAge(realNow) - this.firstCropAfter
+
+    if (this.harvestEvery) {
+      x = x % this.harvestEvery
+    }
+
+    return (x >= 0 && x < this.harvestTime) ? 1 : 0
   }
 }
--- test/shared/plantation.js
+++ test/shared/plantation.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import Plantation from '../../src/shared/plantation'
 import {
   realSecondsToGameDays,
-  gameDaysToRealSeconds
+  gameDaysToRealSeconds as gtr
 } from '../../src/shared/gametime'
 
 test('has age which increases with game time', t => {
@@ -11,17 +11,87 @@ test('has age which increases with game time', t => {
   })
 
   t.is(plantation.getAge(0), 0)
-  t.is(plantation.getAge(gameDaysToRealSeconds(15)), 15)
-  t.is(plantation.getAge(gameDaysToRealSeconds(100)), 100)
+  t.is(plantation.getAge(gtr(15)), 15)
+  t.is(plantation.getAge(gtr(100)), 100)
 })
 
 test('has specific lifespan', t => {
   const plantation = new Plantation({
     date: 0,
-    maxAge: 20
+    maxAge: 20,
+    harvestTime: 5
   })
 
   t.is(plantation.isAlive(0), true)
-  t.is(plantation.isAlive(gameDaysToRealSeconds(20)), true)
-  t.is(plantation.isAlive(gameDaysToRealSeconds(20.1)), false)
+  t.is(plantation.isAlive(gtr(20)), true)
+  t.is(plantation.isAlive(gtr(20.1)), false)
+})
+
+test('becomes harvestable at specified age', t => {
+  const pl = new Plantation({
+    date: 0,
+    maxAge: 20,
+    firstCropAfter: 5,
+    harvestTime: 5
+  })
+
+  function hasCropAt (daysAge) {
+    t.true(pl.getAvailableCrop(gtr(daysAge)) > 0)
+  }
+
+  function hasNoCropAt (daysAge) {
+    t.true(pl.getAvailableCrop(gtr(daysAge)) === 0)
+  }
+
+  hasNoCropAt(0)
+  hasNoCropAt(4.9)
+  hasCropAt(5)
+})
+
+test('is harvestable for a specified time', t => {
+  const pl = new Plantation({
+    date: 0,
+    maxAge: 20,
+    firstCropAfter: 5,
+    harvestTime: 5
+  })
+
+  function hasCropAt (daysAge) {
+    t.true(pl.getAvailableCrop(gtr(daysAge)) > 0)
+  }
+
+  function hasNoCropAt (daysAge) {
+    t.true(pl.getAvailableCrop(gtr(daysAge)) === 0)
+  }
+
+  hasCropAt(4.9 + 5)
+  hasNoCropAt(5 + 5)
+})
+
+test('harvest time repeats periodically', t => {
+  const pl = new Plantation({
+    date: 0,
+    maxAge: 40,
+    firstCropAfter: 5,
+    harvestTime: 5,
+    harvestEvery: 10
+  })
+
+  function hasCropAt (daysAge) {
+    t.true(pl.getAvailableCrop(gtr(daysAge)) > 0)
+  }
+
+  function hasNoCropAt (daysAge) {
+    t.true(pl.getAvailableCrop(gtr(daysAge)) === 0)
+  }
+
+  hasNoCropAt(4.9 + 10)
+  hasCropAt(5 + 10)
+  hasCropAt(5 + 10 + 4.9)
+  hasNoCropAt(5 + 10 + 5)
+
+  hasNoCropAt(4.9 + 20)
+  hasCropAt(5 + 20)
+  hasCropAt(5 + 20 + 4.9)
+  hasNoCropAt(5 + 20 + 5)
 })
