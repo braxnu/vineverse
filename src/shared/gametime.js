5bf68741b1f7dd5932a93ed90eeb4d3376b8fcde
Karol Baczewski <braxnu@gmail.com>
2020-03-22 17:31:27 +0000

    start from scratch

--- src/shared/gametime.js
+++
@@ -1,9 +0,0 @@
-const TIME_SPEED = 12 * 24 // 5 min real time == 24 game time
-
-export function realSecondsToGameDays (realSeconds) {
-  return realSeconds / TIME_SPEED
-}
-
-export function gameDaysToRealSeconds (gameDays) {
-  return gameDays * TIME_SPEED
-}
--- src/shared/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import auth from './auth'
-import deposits from './deposits'
-
-export default combineReducers({
-  auth,
-  deposits
-})
