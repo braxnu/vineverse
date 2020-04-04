ff25c877ae8ee4d82741dcc26a080313debe5cc7
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-04-04 20:03:14 +0200

    Change folder name

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     entry: './src/client/index.js',
     output: {
-        path: path.resolve(__dirname, 'bundle'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
     mode: 'development'
