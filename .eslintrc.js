07078dfef3990cc9a7bf988f0370f62e322826e1
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-04-22 02:02:48 +0200

    Revert "Zmiana liczby spacji z 4 na 2 w: '.eslintrc.js'"
    This reverts commit 9b8d96f796e2a1bd49dd9569144e18c7cc905a62.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            2
+            4
         ],
         "linebreak-style": [
             "error",
--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,11 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const PORT = 3000
 
-const api = require("./routes/api")
+const api = require('./routes/api');
 
-app.use("/", express.static("build"))
+app.use('/', express.static('build'))
 
-app.use("/api", api)
+app.use('/api', api);
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
