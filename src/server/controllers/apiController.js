941b53a2245c9c048938c37eb8c705de316e81cb
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-04-22 02:43:22 +0200

    Dodanie eslinta oraz fix na code style

--- src/server/controllers/apiController.js
+++ src/server/controllers/apiController.js
@@ -1,10 +1,10 @@
-    exports.me = (req, res) => {
-        res.send({ balance: 1000 })
-  };
+exports.me = (req, res) => {
+  res.send({ balance: 1000 })
+}
 
-    exports.stock = (req, res) => {
-        res.send([
-            { name: "Marchew", quantity: 200 },
-            { name: "Przenica", quantity: 500 }
-          ])
-  };
+exports.stock = (req, res) => {
+  res.send([
+    { name: 'Marchew', quantity: 200 },
+    { name: 'Przenica', quantity: 500 }
+  ])
+}
