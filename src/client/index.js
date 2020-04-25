911409ee071ba43f829fde5339a515844f496e0a
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-04-25 13:11:27 +0200

    Instalcaja Reacta

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,8 @@
-console.log(1)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import app from './src/server/app.js';
+
+//console.log(1)
 
 export const DemoComponent = props => (
   <div>{props.name}</div>
