10dece2a2704d723854351cbadcd468c6eae468f
Kneeidle <kneeidle.mail@gmail.com>
2020-04-26 14:13:40 +0200

    Dodany komponent Login z przyciskiem logowania

---
+++ src/client/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <button onClick={() => location.assign('/login')}>Login</button>
+    </div>
+  )
+}
+export default Hello
