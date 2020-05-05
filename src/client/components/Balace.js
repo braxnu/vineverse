98ae0b0fee9bd787085710bfcedc2b5e7427c1d5
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-05-05 02:27:11 +0200

    Dodanie axios w componentach

--- src/client/components/Balace.js
+++ src/client/components/Balace.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class Balance extends React.Component {
   constructor(props) {
     super(props)
+    
     this.state = {
       balance: 0
     }
   }
   
   componentDidMount() {
-    fetch('/api/me', {
-      method: 'GET'
-    })
-      .then(response =>{
-        return response.json()
+    axios.get('/api/me')
+      .then(response => {
+        return response.data
       })
       .then(data => {
         this.setState(data)
