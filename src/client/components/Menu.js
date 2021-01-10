6134ef5b6cbbde7e8646f1886c10318ff1e12e6b
Karol Baczewski <braxnu@gmail.com>
2021-01-10 11:40:53 +0000

    brakujacy setup testow i lint fix

--- src/client/components/Menu.js
+++ src/client/components/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './Menu.css'
 
 const Menu = props => {
@@ -7,8 +8,15 @@ const Menu = props => {
       <div>{item.display}</div>
     </a>
   ))
+
   return (props.screenSize === 'mobile') ?
     (<div className="mobile-nav">{itemList}</div>) :
     (<div className="desktop-nav">{itemList}</div>)
 }
+
+Menu.propTypes = {
+  items: PropTypes.array,
+  screenSize: PropTypes.string,
+}
+
 export default Menu
