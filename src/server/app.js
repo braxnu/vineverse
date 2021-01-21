641767252f3c7ff06203443ad8efe9e4464e2eb1
Kneeidle <kneeidle.mail@gmail.com>
2021-01-22 00:28:09 +0100

    VIN-35 polacznie z mongo

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const config = require('../../config')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 3000
 
@@ -8,4 +10,14 @@ app.use('/', express.static('build'))
 
 app.use('/api', api)
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+;(async () => {
+    app.locals.db = await mongoose.connect(config.mongo.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  
+  app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+    app.listen(PORT, () =>
+      console.log(`Example app listening on port ${PORT}!`)
+    )
+  })()
